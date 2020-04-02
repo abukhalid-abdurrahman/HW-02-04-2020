24221ce1ffba3de80b5b1a40b83ca92fbb79607e
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-02 20:46:30 +0300

    class rectangle was created

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿/*
+ * Developed by Faridun Berdiev
+ * Date: 02.04.2020
+ * First Task
+*/
+using System;
 
 namespace Task_1
 {
+    class Rectangle
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
