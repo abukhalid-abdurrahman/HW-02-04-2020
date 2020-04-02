d43ad5de2ad52ac32061668ca0ad4ff3b64d51a5
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-02 21:40:31 +0300

    Class author, title, content was added

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿/*
+ * Developed by Faridun Berdiev
+ * Date: 02.04.2020
+ * Second Task
+*/
+using System;
 
 namespace Task_2
 {
     class Program
     {
+        class Book
+        {
+            
+        }
+
+        class Title
+        {
+            public string titleStr { get; set; }
+            public void Show()
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Title: " + titleStr);
+            }
+        }
+
+        class Author
+        {
+            public string authorStr { get; set; }
+            public void Show()
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Author: " + authorStr);
+            }
+        }
+
+        class Content
+        {
+            public string сontentStr { get; set; }
+            public void Show()
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Content: " + сontentStr);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
